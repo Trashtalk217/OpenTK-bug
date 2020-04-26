72616f96ad42b58faa9415dee6c2f7348a74f9ec
Trashtalk <Trashtalk217@gmail.com>
2020-04-26 14:02:04 +0200

    first commit

---
+++ template.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using OpenTK;
+
+namespace Template
+{
+    public class OpenTKApp : GameWindow
+    {
+        protected override void OnLoad(EventArgs e)
+        {
+            // Console.WriteLine(Width.ToString() + " " + Height.ToString());
+            // ClientSize = new Size(1200, 800);
+            Console.WriteLine(Width.ToString() + " " + Height.ToString());
+            Height = 800;
+            Width = 1200;
+            Console.WriteLine(Width.ToString() + " " + Height.ToString());
+        }
+
+        public static void Main(string[] args)
+        {
+            // entry point
+            using (OpenTKApp app = new OpenTKApp()) { app.Run(30.0, 0.0); }
+        }
+    }
+}
